5589977f84de7fc0c931c6fabb0bafd52975e261
giovanni vitiello <giovanni.vitiello10@gmail.com>
2019-10-15 13:07:12 +0200

    inserito edit text evidenziato e seconda funzione

--- app/src/main/java/android/corso/it/aboutme/MainActivity.kt
+++ app/src/main/java/android/corso/it/aboutme/MainActivity.kt
@@ -1,8 +1,10 @@
 package android.corso.it.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -20,11 +22,19 @@ class MainActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.done_button)
         button.setOnClickListener{
             addNickName(it)
+
             /*oppure in una singola riga di codice, come sotto, senza creare una variabile*/
 
-//        findViewById<Button>(R.id.done_button).setOnClickListener{
-//            addNickName(it)
+//       findViewById<Button>(R.id.done_button).setOnClickListener{
+//           addNickName(it)
+//       }
+
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+            upDateNickName(it)
         }
+
+        }
+
     }
 
     private fun addNickName(view : View){
@@ -34,6 +44,26 @@ class MainActivity : AppCompatActivity() {
         editText.visibility = View.GONE
         view.visibility = View.GONE
         nicknameTextView.visibility = View.VISIBLE
+
+        //hide the keyboard.
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+
     }
-    
+
+    private fun upDateNickName(view: View){
+        val nicknameEditText = findViewById<EditText>(R.id.nickname_edit)
+        val button = findViewById<Button>(R.id.done_button)
+        nicknameEditText.visibility = View.VISIBLE
+        button.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        // Set the focus to the edit text.
+        //nicknameEditText.requestFocus()
+
+        // Show the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(nicknameEditText, 0)
+    }
+
 }
